844bfe406a3e544b8ac3e309844cfd0f6bebe50a
Aradina <AradinaEmber@gmail.com>
2017-05-17 21:20:08 +0930

    cleaned up slightly.

--- EIT.py
+++ EIT.py
@@ -6,29 +6,29 @@ Created on May 12, 2017
 '''
 from design import Ui_MainWindow
 from esipy import EsiClient, App
-import json
-import sys
-from PyQt5.QtWidgets import QMessageBox, QApplication, QPushButton, QMainWindow
+from json import loads
+from sys import argv
+from PyQt5.QtWidgets import QMessageBox, QApplication, QMainWindow
 from PyQt5.QtGui import QPixmap
-import urllib.request
+from urllib import request
 
-esiApp = App.create('https://esi.tech.ccp.is/latest/swagger.json?datasource=tranquility')
+esiApp = App.create('https://esi.tech.ccp.is/latest/swagger.json?datasource=tranquility') # this establishes the ESI connection.
 esi_client = EsiClient()
 
 class eitMain(QMainWindow):
     def __init__(self):
         super(eitMain, self).__init__()
                 
-        self.ui = Ui_MainWindow()
+        self.ui = Ui_MainWindow()  #Opens the main window.
         self.ui.setupUi(self)
-        self.ui.searchBtn.clicked.connect(self.charSearch)
-        self.ui.actionExit.triggered.connect(QApplication.quit)
+        self.ui.searchBtn.clicked.connect(self.charSearch)  #this is the search button.
+        self.ui.actionExit.triggered.connect(QApplication.quit)    #This is the quit option in the menu bar.
         
-    def charSearch(self):
+    def charSearch(self):   #this passes the text from the search box to the later thing that actually searches.
         self.charName = self.ui.charSearch.text()
         esiFunction.charIntelSearch(self, eitMain)
         
-    def noResults(self):
+    def noResults(self):    #this is displayed if your search has no results, otherwise it would crash.
         noResults = QMessageBox()
         noResults.setIcon(QMessageBox.Information)
         noResults.setText("No Results Found.")
@@ -38,47 +38,38 @@ class eitMain(QMainWindow):
         noResults.exec_()
         self.ui.searchBtn.clicked.connect(self.charSearch)
         
-    def awkward(self):
-        awkward = QMessageBox()
-        awkward.setText("Awkward..")
-        awkward.setWindowTitle("Baka")
-        awkward.addButton(QPushButton('L-lewd'), QMessageBox.NoRole)
-        awkward.exec_()
-        
 class esiFunction():
-    def charIntelSearch(self, eitMain):
-        charSearch = esiApp.op['get_search'](
+    def charIntelSearch(self, eitMain): #the main function. This takes the character name, gets the character ID via the search end point on ESI.
+        charSearch = esiApp.op['get_search'](               #It then uses this to get all the data shown.
             categories = 'character',
             search = self.charName,
             strict = 'true'
             )
-        charIDGet = esi_client.request(charSearch)
-        self.charIDs = json.loads(charIDGet.raw)
-        if 'character' not in self.charIDs:
+        charSearch = esi_client.request(charSearch)
+        self.charID = loads(charSearch.raw)
+        if 'character' not in self.charID:     #Makes sure there are results, if not it causes an error.
             eitMain().noResults()
         else:
+            self.charID = str(self.charID['character'])
+            self.charID = self.charID.strip("[]")
             esiFunction.charIntSucess(self, eitMain)
-    def charIntSucess(self, eitMain):
-        charIDs = self.charIDs
-        charID = str(charIDs['character'])
-        charID = charID.strip("[]")
-        charID = int(charID)
-        charSheetGet = esiApp.op['get_characters_character_id'](
+            
+    def charIntSucess(self, eitMain):   #after getting the character ID, it then gets all the data and sets it. This is that.
+        charID = self.charID
+        charSheet = esiApp.op['get_characters_character_id'](
             character_id = charID
             )
-        charSheet = esi_client.request(charSheetGet)
-        characterSheet = json.loads(charSheet.raw)
-        charName = characterSheet['name']
-        charBio = characterSheet['description']
-        securityStatus = characterSheet['security_status']
-        securityStatus = round(securityStatus, 2)
-        corpID = str(characterSheet['corporation_id'])
-        
+        charSheet = esi_client.request(charSheet)
+        charSheet = loads(charSheet.raw)
+        charBio = charSheet['description']
+        securityStatus = charSheet['security_status']
+        securityStatus = round(securityStatus, 2)   #this rounds the security status to a more useful number. Hopefully.
+        corpID = str(charSheet['corporation_id'])
         corpDetails = esiApp.op['get_corporations_corporation_id'](
             corporation_id = corpID
             )
-        corpNameGet = esi_client.request(corpDetails)
-        corpSheet = json.loads(corpNameGet.raw)
+        corpSheet = esi_client.request(corpDetails)
+        corpSheet = loads(corpSheet.raw)
         corpName = corpSheet['corporation_name']
         corpDescription = corpSheet['corporation_description']
         corpTicker = corpSheet['ticker']
@@ -87,52 +78,37 @@ class esiFunction():
         corpCeoName = esiApp.op['get_characters_character_id'](
             character_id = corpCeo
             )
-        corpCEOWords = esi_client.request(corpCeoName)
-        corpCeoObtainment = json.loads(corpCEOWords.raw)
-        corpCEO = corpCeoObtainment['name']
-        if 'alliance_id' in corpSheet:
+        corpCeo = esi_client.request(corpCeoName)
+        corpCeo = loads(corpCeo.raw)
+        corpCeo = corpCeo['name']
+        if 'alliance_id' in corpSheet:      #This checks to see if the person searched is in an alliance, if they are it gets the logo.
             allianceID = corpSheet['alliance_id']
             allianceimgURL = "https://imageserver.eveonline.com/Alliance/{}_128.png".format( allianceID )
-            allianceurl = urllib.request.urlopen(allianceimgURL).read()
+            allianceurl = request.urlopen(allianceimgURL).read()
             pixmapAlliance = QPixmap()
             pixmapAlliance.loadFromData(allianceurl)
             self.ui.allianceLogo.setPixmap(pixmapAlliance)
             self.ui.allianceLogo_2.setPixmap(pixmapAlliance)
             
         charimgURL = "https://imageserver.eveonline.com/Character/{}_128.jpg".format( charID )
-        charurl = urllib.request.urlopen(charimgURL).read()
+        charurl = request.urlopen(charimgURL).read()
         pixmapChar = QPixmap()
         pixmapChar.loadFromData(charurl)
         self.ui.charLogo.setPixmap(pixmapChar)
         
         corpimgURL = "https://imageserver.eveonline.com/Corporation/{}_128.png".format( corpID )
-        corpurl = urllib.request.urlopen(corpimgURL).read()
+        corpurl = request.urlopen(corpimgURL).read()
         pixmapCorp = QPixmap()
         pixmapCorp.loadFromData(corpurl)
         self.ui.corpLogo.setPixmap(pixmapCorp)
         self.ui.corpLogo_2.setPixmap(pixmapCorp)
-        
-        if 'Tsao Aubbes' in charName:
-            eitMain().awkward()
         self.ui.charName.setText('Name: '+self.charName)
         self.ui.charName_2.setText('Name: '+self.charName)
-        self.ui.corpCeo.setText('CEO: '+corpCEO)
+        self.ui.corpCeo.setText('CEO: '+corpCeo)
         self.ui.corpName.setText('Corporation Name: '+corpName+' : '+corpTicker)
         self.ui.corpName_2.setText('Corporation Name: '+corpName+' : '+corpTicker)
         self.ui.corpMembers.setText('Member Count: '+str(corpMemberCount))
         self.ui.corpMembers_2.setText('Member Count: '+str(corpMemberCount))
-        """
-        if charID == corpCeo:
-            print("CEO")
-        else:
-            corpCeoGet = self.esiApp.op['get_characters_character_id'](
-                character_id = corpCeo
-                )
-            corpCeoGetter = self.esi_client.request(corpCeoGet)
-            corpCeoOther = json.loads(corpCeoGetter.raw)
-            corpCeoOther = corpCeoOther['name']
-            print(corpCeoOther)
-        """
         self.ui.charBio.setText(charBio)
         self.ui.charBio_2.setText(charBio)
         self.ui.charBio.setWordWrap(True)
@@ -143,7 +119,7 @@ class esiFunction():
         self.ui.secStatus_2.setText('Security Status: '+str(securityStatus))
         
 def main():
-    app = QApplication(sys.argv)
+    app = QApplication(argv)
     form = eitMain()
     form.show()
     app.exec_()
